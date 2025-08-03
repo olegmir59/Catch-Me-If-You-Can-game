3264f12d809799af9ad45d5044b7cb920c786ca8
olegmir59 <udom1@ya.ru>
2025-08-04 02:51:16 +0700

    add JPG files and modify catch_me.py

--- catch_me.py
+++ catch_me.py
@@ -21,13 +21,18 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-#
+crow_img = pygame.image.load("img/Ворон2.jpg")  # {{ edit_1 }}
+crow_img = pygame.transform.scale(crow_img, (400, 400))  # {{ edit_1 }}
+crow_x = random.randint(0, SCREEN_WIDTH - 400)  # {{ edit_1 }}
+crow_y = random.randint(0, SCREEN_HEIGHT - 400)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 # Флаг для проверки, нажата ли мышь
 start_game = False
+catch_count = 0
 
 # Основной цикл
 while True:
@@ -52,10 +57,23 @@ while True:
                 running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            # Проверка попадания по бабочке
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
+                crow_x = random.randint(0, SCREEN_WIDTH - 400)
+                crow_y = random.randint(0, SCREEN_HEIGHT - 400)
+                catch_count += 10
+                print(catch_count)
+            # Проверка попадания по ворону {{ edit_2 }}
+            if crow_x < mouse_x < crow_x + 400 and crow_y < mouse_y < crow_y + 400:
+                crow_x = random.randint(0, SCREEN_WIDTH - 400)
+                crow_y = random.randint(0, SCREEN_HEIGHT - 400)
+                catch_count -= 50
+                print(catch_count)
+        # Отрисовка ворона {{ edit_3 }}
+        screen.blit(crow_img, (crow_x, crow_y))
+        # Отрисовка бабочки
         screen.blit(target_img, (target_x, target_y))
         pygame.display.update()
 
